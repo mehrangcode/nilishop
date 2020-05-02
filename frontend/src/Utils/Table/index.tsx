ab1d35ff416adece25605815610ea388d12b48a8
Mehran <imganjgahi@gmail.com>
2020-05-02 17:54:49 +0430

    table style

--- frontend/src/Utils/Table/index.tsx
+++ frontend/src/Utils/Table/index.tsx
@@ -10,45 +10,42 @@ export const Table = (props: IProps) => {
         return null
     }
     return (
-        <div className="table">
-            {
-                React.Children.map(props.children, (child: any) => {
-                    if (child) {
-                        if (child.type === Column) {
-                            
-                            console.log("name: ", child.props.name)
-                            return <div className="column">
-                                <p className="columnTitle">
-                                    {child.props.title}
-                                </p>
-                                {props.data.map(item => {
-                                    if(child.props.render){
-                                        return <p className="columnData">
-                                            {child.props.render(item)}
-                                        </p>
-                                    }
-                                    let itemStr = ""
-                                    if(item[child.props.dataName]) {
-                                        itemStr = item[child.props.dataName]
-                                        if(itemStr.length > 40){
-                                            itemStr = item[child.props.dataName].substring(0, 50) + "..."
-                                        }
-                                    }
-                                    return <p className="columnData"> {itemStr} </p>
-                                })}
-                            </div>
+        <table>
+            <tr>
+                    {React.Children.map(props.children, (child: any) => {
+                        if (child) {
+                            if (child.type === Column) {
+                                console.log("name: ", child.props.name)
+                                return (
+                                    <th>
+                                        {child.props.title}
+                                    </th>)
+                            }
                         }
-                    }
-                    return null
-                })
-            }
-
-        </div>
+                    })}</tr>
+            {props.data.map(item => {
+                return <tr>
+                    {React.Children.map(props.children, (child: any) => {
+                        if (child) {
+                            if (child.type === Column) {
+                                if(child.props.render){
+                                    return <td> {child.props.render(item)} </td> 
+                                }
+                                if (item[child.props.dataName]) {
+                                    let str = item[child.props.dataName];
+                                    if(item[child.props.dataName].length > 60) {
+                                        str = item[child.props.dataName].substring(0, 60)+"..."
+                                    }
+                                    return <td> {str} </td>
+                                }
+                                return <td></td>
+                            }
+                        }
+                    })}
+                </tr>
+            })}
+        </table>
     )
 }
 
-export class Column extends React.Component<any, any>{
-    // render() {
-    //     return <p>{this.props.name}</p>;
-    // }
-}
+export class Column extends React.Component<any, any>{}
