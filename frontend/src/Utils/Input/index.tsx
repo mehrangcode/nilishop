e0aaaa1d529b90d29a1c72e7bbf4a1d8bcf38959
Mehran <imganjgahi@gmail.com>
2020-05-03 19:31:54 +0430

    input text design

--- frontend/src/Utils/Input/index.tsx
+++ frontend/src/Utils/Input/index.tsx
@@ -14,31 +14,32 @@ const Input = (props: IProps) => {
     const [inputValue, setValue] = useState<string | undefined>(undefined)
 
     useEffect(() => {
-        if(props.initialvalue){
+        if (props.initialvalue) {
             setValue(props.initialvalue)
         }
-        
+
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
-    useEffect(()=> {
-        if(props.onChange){
-            if(inputValue || inputValue === ""){
+    useEffect(() => {
+        if (props.onChange) {
+            if (inputValue || inputValue === "") {
                 props.onChange(inputValue)
             }
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[inputValue])
+    }, [inputValue])
 
     const onchangeHandler = (event: any) => {
         event.preventDefault();
         setValue(event.target.value)
     }
     return (
-        <input 
+        <input
+            className={props.className ? props.className : "input"}
             value={inputValue}
             onChange={onchangeHandler}
-            id={props.id} 
-            type={props.type ? props.type : "text"} 
+            id={props.id}
+            type={props.type ? props.type : "text"}
             placeholder={props.placeholder ? props.placeholder : ""} />
     )
 }
